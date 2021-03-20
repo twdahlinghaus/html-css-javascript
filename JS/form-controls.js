1a410f6c794dd79fd0837b72f7bbce87122b1513
Thomas Dahlinghaus <twdahlinghaus@gmail.com>
2021-03-19 23:13:10 -0400

    more html css practice

---
+++ JS/form-controls.js
@@ -0,0 +1,7 @@
+function button_click() {
+    let inCtrl = document.getElementById("inp");
+    let inValue = inCtrl.value;
+    let outCtrl = document.getElementById("outp");
+    outCtrl.value = inValue;
+    
+}
